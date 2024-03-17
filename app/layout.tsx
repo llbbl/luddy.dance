476a1293c0bda085d2c85a808b22c76d79564f69
llbbl <logan@llbbl.com>
2024-03-16 20:44:55 -0500

    adding seo image

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout( {
     <SEO
       title={String(metadata.title || '')}
       description={String(metadata.description || '')}
-      image="/10hours-luddy.png"
+      image="//10hours-luddy.webp"
     />
     <body className={ inter.className }>
     <div>
